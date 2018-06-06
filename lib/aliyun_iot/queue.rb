39ecd390d53c07c941f8400e9d2af0fcc01e2ade
garrus <garrus1118@qq.com>
2018-06-06 18:22:23 +0800

    fix content bug

--- lib/aliyun_iot/queue.rb
+++ lib/aliyun_iot/queue.rb
@@ -36,7 +36,7 @@ module AliyunIot
             :MessageRetentionPeriod => 345600,
             :PollingWaitSeconds => 0
         }.merge(opts)
-        request.content_xml(:Queue, msg_options)
+        request.content(:Queue, msg_options)
       end
     end
 
@@ -57,7 +57,7 @@ module AliyunIot
             :DelaySeconds => 0,
             :Priority => 8
         }.merge(opts)
-        request.content_xml(:Message, msg_options.merge(:MessageBody => message.to_s))
+        request.content(:Message, msg_options.merge(:MessageBody => message.to_s))
       end
     end
 
@@ -73,6 +73,13 @@ module AliyunIot
       result = Request::Xml.get(messages_path, query: {waitseconds: wait_seconds, numOfMessages: num})
       return nil if result.nil?
       Result.new(self, result, "Messages", "Message").get_message
+    end
+    
+    #设置队列属性
+    def set_attr(opts = {})
+      Request::Xml.put(queue_path, query: {Metaoverride: true}) do |request|
+        request.content(:Queue, opts)
+      end
     end
 
     def queue_path
